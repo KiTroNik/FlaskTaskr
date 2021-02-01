2924c99d143904b9c0ddca48d55f29482e12a8fd
Jakub <kubatom3@gmail.com>
2021-02-01 20:32:35 +0100

    registration completed

--- project/forms.py
+++ project/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import StringField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class AddTaskForm(Form):
@@ -12,4 +12,34 @@ class AddTaskForm(Form):
                                 ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
                                 ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')
                             ]
+    )
+
+
+class RegisterForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        validators=[DataRequired(), EqualTo('password', message='Passwords must match')]
+    )
+
+
+class LoginForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
     )
--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
+from forms import AddTaskForm, RegisterForm, LoginForm
 from functools import wraps
-from flask import Flask, flash, redirect, render_template, request, session, url_for, g
-from forms import AddTaskForm
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -10,7 +10,7 @@ app.config.from_object('_config')
 db = SQLAlchemy(app)
 
 
-from models import Task
+from models import Task, User
 
 
 # helpers
@@ -38,18 +38,19 @@ def logout():
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] or \
-                request.form['password'] != app.config['PASSWORD']:
-
-            error = 'Invalid Credentials. Please try again.'
-            return render_template('login.html', error=error)
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=request.form['name']).first()
+            if user is not None and user.password == request.form['password']:
+                session['logged_in'] = True
+                flash('Welcome!')
+                return redirect(url_for('tasks'))
+            else:
+                error = 'Invalid username or password'
         else:
-            session['logged_in'] = True
-            flash('Welcome!')
-            return redirect(url_for('tasks'))
-
-    return render_template('login.html')
+            error = 'Both fields are required'
+    return render_template('login.html', form=form, error=error)
 
 
 @app.route('/tasks/')
@@ -104,3 +105,21 @@ def delete_entry(task_id):
     db.session.commit()
     flash('The task was deleted. Why not add a new one?')
     return redirect(url_for('tasks'))
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                form.name.data,
+                form.email.data,
+                form.password.data
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Thanks for registering. Please login.')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form, error=error)
