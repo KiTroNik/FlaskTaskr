d6562563c56f85bf4e462c83f6b5e92aecb9091b
Jakub Tomala <kubatom3@gmail.com>
2021-02-23 17:42:56 +0100

    admin role added

--- project/test_tasks.py
+++ project/test_tasks.py
@@ -50,6 +50,17 @@ class TasksTests(unittest.TestCase):
         db.session.commit()
 
 
+    def create_admin_user(self):
+        new_user = User(
+            name='Superman',
+            email='admin@realpython.com',
+            password='allpowerfull',
+            role='admin'
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+
     def create_task(self):
         return self.app.post('add/', data=dict(
             name='Go to the bank',
@@ -148,6 +159,36 @@ class TasksTests(unittest.TestCase):
             b'You can only delete tasks that belong to you.', response.data
         )
     
+
+    def test_admin_users_can_complete_tasks_that_are_not_created_by_them(self):
+        self.create_user('Michael', 'michael@realpython.com', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.create_admin_user()
+        self.login('Superman', 'allpowerfull')
+        self.app.get('tasks/', follow_redirects=True)
+        response = self.app.get('complete/1/', follow_redirects=True)
+        self.assertNotIn(
+            b'You can only update tasks that belong to you.', response.data
+        )
+
+    
+    def test_admin_user_can_delete_tasks_that_are_not_created_by_them(self):
+        self.create_user('Michael', 'michael@realpython.com', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.create_admin_user()
+        self.login('Superman', 'allpowerfull')
+        self.app.get('tasks/', follow_redirects=True)
+        response = self.app.get('delete/1/', follow_redirects=True)
+        self.assertNotIn(
+            b'You can only delete tasks that belong to you.', response.data
+        )
+
 
 
 if __name__ == '__main__':
--- project/views.py
+++ project/views.py
@@ -51,6 +51,7 @@ def closed_tasks():
 def logout():
     session.pop('logged_in', None)
     session.pop('user_id', None)
+    session.pop('role', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
 
@@ -65,6 +66,7 @@ def login():
             if user is not None and user.password == request.form['password']:
                 session['logged_in'] = True
                 session['user_id'] = user.id
+                session['role'] = user.role
                 flash('Welcome!')
                 return redirect(url_for('tasks'))
             else:
@@ -118,7 +120,7 @@ def new_task():
 def complete(task_id):
     new_id = task_id
     task = db.session.query(Task).filter_by(task_id=new_id)
-    if session['user_id'] == task.first().user_id:
+    if session['user_id'] == task.first().user_id or session['role'] == 'admin':
         task.update({"status": "0"})
         db.session.commit()
         flash('The task is complete. Nice.')
@@ -134,7 +136,7 @@ def complete(task_id):
 def delete_entry(task_id):
     new_id = task_id
     task = db.session.query(Task).filter_by(task_id=new_id)
-    if session['user_id'] == task.first().user_id:
+    if session['user_id'] == task.first().user_id or session['role'] == 'admin':
         task.delete()
         db.session.commit()
         flash('The task was deleted. Why not add a new one?')
