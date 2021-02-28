1acb4ba9da3c121171c3d2dc8da63b452a39fb9a
Jakub <kubatom3@gmail.com>
2021-02-28 15:56:08 +0100

    username showup repaired

--- project/tasks/views.py
+++ project/tasks/views.py
@@ -46,8 +46,7 @@ def tasks():
         'tasks.html',
         form=AddTaskForm(request.form),
         open_tasks=open_tasks(),
-        closed_tasks=closed_tasks(),
-        username=session['name']
+        closed_tasks=closed_tasks()
     )
 
 
