73ce8a9cb0e1ba56db59a9581ec6211509fbeef9
Jakub <kubatom3@gmail.com>
2021-03-02 18:33:15 +0100

    500 custom error page

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+import datetime
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -17,5 +18,23 @@ app.register_blueprint(tasks_blueprint)
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write('\n404 error at {}: {}'.format(current_timestamp, r))
+        return render_template('404.html'), 404
 
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write('\n500 error at {}: {}'.format(current_timestamp, r))
+    return render_template('500.html'), 500
+    
--- tests/test_main.py
+++ tests/test_main.py
@@ -17,10 +17,13 @@ class MainTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
+
+        self.assertEquals(app.debug, False)
 
     # executed after each test
     def tearDown(self):
@@ -36,10 +39,16 @@ class MainTests(unittest.TestCase):
         return self.app.post('/', data=dict(
             name=name, password=password), follow_redirects=True)
 
+
+    ########################
+    ####      tests     ####
+    ########################  
+
     def test_404_error(self):
         response = self.app.get('/this_route_dont_exists/')
         self.assertEqual(response.status_code, 404)
         self.assertIn(b'Sorry. There\'s nothing here.', response.data)
+        
 
     def test_500_error(self):
         bad_user = User(
@@ -49,10 +58,12 @@ class MainTests(unittest.TestCase):
         )
         db.session.add(bad_user)
         db.session.commit()
-        response = self.login('Jeremy', 'django')
-        self.assertEqual(response.status_code, 500)
-        self.assertNotIn(b'Invalid salt', response.data)
-        self.assertIn(b'Something went terribly wrong.', response.data)
+        self.assertRaises(ValueError, self.login, 'Jeremy', 'django')
+        try:
+            response = self.login('Jeremy', 'django')
+            self.assertEquals(response.status_code, 500)
+        except ValueError:
+            pass
 
 
 if __name__ == "__main__":
