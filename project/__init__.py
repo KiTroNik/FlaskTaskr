f34399f3d8c0203a3b81854bd8e59d9b41679a3d
Jakub <kubatom3@gmail.com>
2021-03-03 19:09:44 +0100

    restored

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 import datetime
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import Bcrypt
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt
 
 
 app = Flask(__name__)
